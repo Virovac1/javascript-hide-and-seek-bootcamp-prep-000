b8aeb27b0037bfcc72ae8ee4e486c05240281535
William Spangler <bspangs@hotmail.com>
2018-01-21 21:56:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ rankedList[i].innerHTML = ( parseInt(rankedList[i].innerHTML) + n).toString();
 //idea: use querySelectorAll to generate what will be maximum length
 function deepestChild(){
 var grandNode = document.getElementById('grand-node');
- grandNode.lastChild;
+return grandNode.lastChild;
 
 }
