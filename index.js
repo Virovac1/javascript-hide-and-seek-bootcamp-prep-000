36bb9601f507088ebeb05cd9b33acbdf054e3cee
William Spangler <bspangs@hotmail.com>
2018-01-21 21:15:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ rankedList[i].innerHTML = ( parseInt(rankedList[i].innerHTML) + n).toString();
 //idea: use querySelectorAll to generate what will be maximum length
 deepestChild(){
 var grandNode = document.getElementById('grand-node');
- grandNode.lastChild;
+ return grandNode.lastChild;
 }
 /// how do I get/gather/call on the whole group of divs beforehand? Maybe 
