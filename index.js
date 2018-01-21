571209ce9ae9069ff2317201af737c2fd6b1c19c
William Spangler <bspangs@hotmail.com>
2018-01-21 22:29:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ rankedList[i].innerHTML = ( parseInt(rankedList[i].innerHTML) + n).toString();
 
 //idea: use querySelectorAll to generate what will be maximum length
 function deepestChild(){
-var grandNode = document.getElementById('grand-node');
-return grandNode;
-
+const depthList = document.getElementById('grand-node').querySelectorAll('div')
+//after length, could save lenth as a variable and then maybe do depthList.getElementsByTagName('div')[length variable]
+const d= depthList.length;
+return depthList[d-1];
 }
+
+
